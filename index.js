e1d2a95420ff403453882564fffc38a9af100dfe
vincey101 <vincentoladimeji@gmail.com>
2024-09-18 08:20:20 +0100

    cleaning up index file

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const videoRoutes = require('./routes/videoRoutes'); // Assuming your routes file is called videoRoutes.js
+const videoRoutes = require('./routes/videoRoutes'); 
 require('dotenv').config();
 
 const app = express();
